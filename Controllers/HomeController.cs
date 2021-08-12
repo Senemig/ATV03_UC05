16fd414d5397b81caff3f2f4c419b4c801a3dbb5
senemig <fernandogimenes89@gmail.com>
2021-08-12 19:25:20 -0300

    primeiro commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Atividade3.Models;
+
+namespace Atividade3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Serviços()
+        {
+            return View();
+        }
+
+        public IActionResult Contato()
+        {
+            return View();
+        }
+
+        public IActionResult SalvarMensagem(Mensagem m)
+        {
+            return View("ConfirmarMsg", m);
+        }
+    }
+}
---
+++ Models/Mensagem.cs
@@ -0,0 +1,12 @@
+namespace Atividade3.Models
+{
+    public class Mensagem
+    {
+        public string mensagem {get; set;}
+        public string nome {get; set;}
+        public string email {get; set;}
+        public string telefone {get; set;}
+        public string animal {get; set;}
+        public int tipoMensagem {get; set;}
+    }
+}
